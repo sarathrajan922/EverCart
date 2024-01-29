66cc252c523e4c30dd2f0941cd606521cd00b012
sarathrajan922 <sarathrajan922@gmail.com>
2024-01-29 22:49:40 +0530

    config file added in client side

---
+++ client/app/login/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Login:React.FC = ()=>{
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between  bg-white">
+        This is Login Component!
+      </main>
+    )
+}
+
+export default Login;
---
+++ client/config.ts
@@ -0,0 +1,10 @@
+const BASE_URL = 'http://localhost:8800/api/';
+
+export const urls = {
+    USER_REGISTER : 'auth/signup',
+    USER_LOGIN : 'auth/login',
+    CREATE_QUIZ : 'user/quiz'
+}
+
+export default BASE_URL;
+
